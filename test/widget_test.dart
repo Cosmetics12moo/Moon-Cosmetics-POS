b33b006e11dd0f02c2beb0218115e8131b828a55
Cosmetics12moo <moonrehmani0000000@gmail.com>
2026-09-04 08:17:41 +0500

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:moon_cosmetics_pos/app.dart';
-
-void main() {
-  testWidgets('Moon Cosmetics POS app starts', (WidgetTester tester) async {
-    await tester.pumpWidget(const MoonCosmeticsApp());
-    expect(find.text('POS / Sales'), findsOneWidget);
-  });
-}
